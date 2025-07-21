ab8cb38e3acf2bfdfb5b2098f0a199f6f6a87b85
Sattarkhan37 <abdulsattarkhan973@gmail.com>
2025-07-21 18:21:25 +0530

    worked on Input Field (controlled Elements )

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: false },
@@ -17,19 +19,34 @@ function Logo() {
   return <h1>🌴🌴 Far Away 💼💼</h1>;
 }
 function Form() {
+  const [description, setdecripction] = useState("");
+  const [quantity, setquantity] = useState(1);
   function handelSubmit(e) {
     e.preventDefault();
+    if (!description) return;
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    console.log(newItem);
+    setdecripction("");
+    setquantity("");
   }
   return (
     <form className="add-form" onSubmit={handelSubmit}>
       <h3>What doi you need for your 😍 trip?</h3>
-      <select>
+      <select
+        value={quantity}
+        onChange={(e) => setquantity(Number(e.target.value))}
+      >
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
           <option value={num}>{num}</option>
         ))}
       </select>
-      <input type="text" placeholder="Item.." />
-      <button>Add Item</button>
+      <input
+        type="text"
+        placeholder="Item.."
+        value={description}
+        onChange={(e) => setdecripction(e.target.value)}
+      />
+      <button>Add</button>
     </form>
   );
 }
